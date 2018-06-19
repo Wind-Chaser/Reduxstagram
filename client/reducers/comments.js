4268726241897e782a8cc5215d53c307358fb24d
Wind-Chaser <ranaasr60@gmail.com>
2018-06-19 13:19:05 +0530

    [FEATURE]:Remove comment added

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,30 @@
+function postComments (state = [],action) {
+    switch (action.type){
+        case 'ADD_COMMENT' : 
+            return [...state,{
+                    user:action.author,
+                    text:action.comment
+            }];
+        case 'REMOVE_COMMENT' : 
+            return [
+                ...state.slice(0,action.i),
+                ...state.slice(action.i +  1)
+            ]
+        default :
+            return state;
+    }
+}
+
 function comments (state = [],action) {
-    return state;
+    if(typeof action.postId !== 'undefined'){
+
+        return {
+            // Object.assign({},state,{[action.postId]: postComments(state[action.postId],action)});
+               ...state,
+               [action.postId]: postComments(state[action.postId],action)
+        }
+    }
+    return state
 }
 
 export default comments;
