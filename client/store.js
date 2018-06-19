bee8221825bcc3686c30c1cb8ad20c45ed2aa55a
Wind-Chaser <ranaasr60@gmail.com>
2018-06-19 15:36:21 +0530

    [FEATURE]:Redux dev tools and reducers hot reload

--- client/store.js
+++ client/store.js
@@ -10,8 +10,21 @@ const defaultStore = {
     posts,
     comments
 }
+let c;
+compose(f => console.log(f));
+debugger;
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
-const store = createStore(rootReducer,defaultStore);
+const store = createStore(rootReducer, defaultStore, enhancers);
+
+if(module.hot) {
+    module.hot.accept('./reducers', () => {
+        const nextRootReducer = require('./reducers/index').default;
+        store.replaceReducer(nextRootReducer)
+    });
+}
 
 export const history = syncHistoryWithStore(browserHistory,store);
 
